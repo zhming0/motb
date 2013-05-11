1f0eb6a4f961da8697cf018716ddcf2f3e8cfd9d
Ming <mjzshd@gmail.com>
2013-05-11 22:03:27 +0800

    Slightly modify the codebase

--- src/motb/core.clj
+++ src/motb/core.clj
@@ -1,12 +1,16 @@
 (ns motb.core
   (:use [ring.adapter.jetty :only (run-jetty)]
         [compojure.core :only [defroutes routes GET]]
-        [compojure.route :only [not-found resources]]))
+        [compojure.route :only [not-found resources]]
+        [compojure.handler :only [site]]
+        [ring.util.response :only [resource-response]]))
 
-(defroutes app 
-  (GET "/hello/:foo" {{foo :foo} :params}
-    (str "Foo = " foo))
-  (resources "")
-  (not-found "Are you kidding me? "))
+(defroutes main-routes 
+  (GET "/" [] (resource-response "index.html" {:root "public"}))
+  (resources "/")
+  (not-found "Page not found"))
+
+(def app
+  (-> (site main-routes)))
 
 ;(def server (run-jetty #'app {:port 8080 :join? false}))
